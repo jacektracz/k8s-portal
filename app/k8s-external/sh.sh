db4a5de75ee4e308559f1ad4b60748bec1bf67de
Jacek Tracz <jacek_tracz@wp.pl>
2023-06-09 16:20:22 +0200

    1905

--- app/k8s-external/sh.sh
+++ app/k8s-external/sh.sh
@@ -1,2 +1,4 @@
 microk8s kubectl apply -f depl.yaml
 microk8s kubectl apply -f sv-10905.yaml
+microk8s kubectl port-forward -n default service/nginx-svc-10905 30012:10905 --address 146.59.19.222
+netstat -tlpn| grep nginx
